ca8c9bb2726a16ec034154fe4da140aecb8d5cd8
yuanchen <yuanchenshi@gmail.com>
2020-02-01 19:45:55 -0600

    Last Occurrence

---
+++ src/algorithm_class/binary_search/LastOccurrence.java
@@ -0,0 +1,48 @@
+package algorithm_class.binary_search;
+
+// Given a target integer T and an integer array A sorted in ascending order, find the index of the
+// last occurrence of T in A or return -1 if there is no such index.
+//
+// Assumptions
+//
+// There can be duplicate elements in the array.
+//
+// Examples
+//
+// A = {1, 2, 3}, T = 2, return 1
+// A = {1, 2, 3}, T = 4, return -1
+// A = {1, 2, 2, 2, 3}, T = 2, return 3
+// Corner Cases
+//
+// What if A is null or A is array of zero length? We should return -1 in this case.
+public class LastOccurrence {
+  public static int lastOccur(int[] array, int target) {
+    if (array == null || array.length == 0) {
+      return -1;
+    }
+    int left = 0, right = array.length - 1;
+    while (left < right - 1) {
+      int mid = left + (right - left) / 2;
+      if (array[mid] <= target) {
+        left = mid;
+      } else {
+        right = mid;
+      }
+    }
+    if (array[right] == target) {
+      return right;
+    } else if (array[left] == target) {
+      return left;
+    } else {
+      return -1;
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] array1 = new int[]{1, 2, 3};
+    int[] array2 = new int[]{1, 2, 2, 2, 3};
+    System.out.println(lastOccur(array1, 2));
+    System.out.println(lastOccur(array1, 4));
+    System.out.println(lastOccur(array2, 2));
+  }
+}
