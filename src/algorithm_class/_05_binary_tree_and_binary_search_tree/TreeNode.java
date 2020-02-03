94fe30e82a3fc25d6f1763d196baa4609f16d38a
yuanchen <yuanchenshi@gmail.com>
2020-02-03 10:23:53 -0600

    TreeNode

---
+++ src/algorithm_class/_05_binary_tree_and_binary_search_tree/TreeNode.java
@@ -0,0 +1,12 @@
+package algorithm_class._05_binary_tree_and_binary_search_tree;
+
+public class TreeNode {
+
+  public int key;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int key) {
+    this.key = key;
+  }
+}
