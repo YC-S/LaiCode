2c856ae889cd5c60105e709594f18abb8f3fea64
yuanchen <yuanchenshi@gmail.com>
2020-02-03 00:20:58 -0600

    Sort With 2 Stacks

---
+++ src/algorithm_class/_04_queue_stack/QueueByTwoStacks.java
@@ -0,0 +1,48 @@
+package algorithm_class._04_queue_stack;
+
+import java.util.LinkedList;
+
+// Implement a queue by using two stacks. The queue should provide size(), isEmpty(), offer(),
+// poll() and peek() operations. When the queue is empty, poll() and peek() should return null;
+public class QueueByTwoStacks {
+
+  // always insert into the in stack
+  // always remove from the out stack
+  private LinkedList<Integer> in;
+  private LinkedList<Integer> out;
+
+  public QueueByTwoStacks() {
+    in = new LinkedList<>();
+    out = new LinkedList<>();
+  }
+
+  public Integer poll() {
+    move();
+    return out.isEmpty() ? null : out.pollFirst();
+  }
+
+  public void move() {
+    if (out.isEmpty()) {
+      while (!in.isEmpty()) {
+        out.offerFirst(in.pollFirst());
+      }
+    }
+  }
+
+  public void offer(int element) {
+    in.offerFirst(element);
+  }
+
+  public Integer peek() {
+    move();
+    return out.isEmpty() ? null : out.peekFirst();
+  }
+
+  public int size() {
+    return in.size() + out.size();
+  }
+
+  public boolean isEmpty() {
+    return in.size() + out.size() == 0;
+  }
+}
