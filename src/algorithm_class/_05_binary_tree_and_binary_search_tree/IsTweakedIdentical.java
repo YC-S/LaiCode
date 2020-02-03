899bcc0afbb3d30a9fc0297abcfb6696db208411
yuanchen <yuanchenshi@gmail.com>
2020-02-03 11:29:56 -0600

    Cheack if two Binary trees are tweaked identical

--- src/algorithm_class/_05_binary_tree_and_binary_search_tree/IsTweakedIdentical.java
+++ src/algorithm_class/_05_binary_tree_and_binary_search_tree/IsTweakedIdentical.java
@@ -1,5 +1,34 @@
 package algorithm_class._05_binary_tree_and_binary_search_tree;
 
+// Determine whether two given binary trees are identical assuming any number of ‘tweak’s are
+// allowed. A tweak is defined as a swap of the children of one node in the tree.
+//
+// Examples
+//
+//        5
+//
+//      /    \
+//
+//    3        8
+//
+//  /   \
+//
+// 1      4
+//
+// and
+//
+//        5
+//
+//      /    \
+//
+//    8        3
+//
+//           /   \
+//
+//          1     4
+//
+// the two binary trees are tweaked identical.
+
 public class IsTweakedIdentical {
 
   public static boolean isTweakedIdentical(TreeNode one, TreeNode two) {
