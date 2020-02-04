ed8a4725d973c8bb14af252b296288e0ab2dbc02
yuanchen <yuanchenshi@gmail.com>
2020-02-04 13:59:20 -0600

    Layer By Layer Binary Tree

---
+++ src/algorithm_class/_06_BFS/LayerByLayer.java
@@ -0,0 +1,68 @@
+package algorithm_class._06_BFS;
+
+// Get the list of list of keys in a given binary tree layer by layer. Each layer is represented by a list of keys and the keys are traversed from left to right.
+//
+//Examples
+//
+//        5
+//
+//      /    \
+//
+//    3        8
+//
+//  /   \        \
+//
+// 1     4        11
+//
+//the result is [ [5], [3, 8], [1, 4, 11] ]
+// Corner Cases
+//
+//What if the binary tree is null? Return an empty list of list in this case.
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LayerByLayer {
+
+  public static List<List<Integer>> layerByLayer(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      List<Integer> curLayer = new ArrayList<>();
+      int n = queue.size();
+      for (int i = 0; i < n; i++) {
+        TreeNode cur = queue.poll();
+        curLayer.add(cur.key);
+        if (cur.left != null) {
+          queue.offer(cur.left);
+        }
+        if (cur.right != null) {
+          queue.offer(cur.right);
+        }
+      }
+      res.add(curLayer);
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    TreeNode t1 = new TreeNode(5);
+    TreeNode t2 = new TreeNode(3);
+    TreeNode t3 = new TreeNode(8);
+    TreeNode t4 = new TreeNode(1);
+    TreeNode t5 = new TreeNode(4);
+    TreeNode t6 = new TreeNode(11);
+    t1.left = t2;
+    t1.right = t3;
+    t2.left = t4;
+    t2.right = t5;
+    t3.right = t6;
+    System.out.println(layerByLayer(t1));
+  }
+}
---
+++ src/algorithm_class/_06_BFS/TreeNode.java
@@ -0,0 +1,12 @@
+package algorithm_class._06_BFS;
+
+public class TreeNode {
+
+  public int key;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int key) {
+    this.key = key;
+  }
+}
