47780b3f80470271a12370c18f7887d2cf904ba3
yuanchen <yuanchenshi@gmail.com>
2020-03-01 15:25:45 -0600

    InOrderIterative

---
+++ src/practice/class_24_problems_in_practice/CalculateMedian.java
@@ -0,0 +1,9 @@
+package practice.class_24_problems_in_practice;
+
+public class CalculateMedian {
+
+  public int getMedian(int[] array) {
+    int n = array.length;
+    return array[n / 2];
+  }
+}
---
+++ src/practice/class_24_problems_in_practice/CalculateMedianTest.java
@@ -0,0 +1,9 @@
+package practice.class_24_problems_in_practice;
+
+class CalculateMedianTest {
+
+  @org.junit.jupiter.api.Test
+  void getMedian() {
+//    fail("Not yet implemented");
+  }
+}
