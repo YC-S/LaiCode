e36068a684d5feef14f00bd22e452cd66d4d2fb7
yuanchen <yuanchenshi@gmail.com>
2020-03-01 15:29:11 -0600

    PostOrderIterative

---
+++ src/algorithm_class/_05_binary_tree_and_binary_search_tree/PostOrderIterative.java
@@ -0,0 +1,47 @@
+package algorithm_class._05_binary_tree_and_binary_search_tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+
+public class PostOrderIterative {
+
+  public static List<Integer> postOrder(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    if (root == null) {
+      return res;
+    }
+    stack.offerFirst(root);
+    while (!stack.isEmpty()) {
+      TreeNode cur = stack.pollFirst();
+      res.add(cur.key);
+      if (cur.left != null) {
+        stack.offerFirst(cur.left);
+      }
+      if (cur.right != null) {
+        stack.offerFirst(cur.right);
+      }
+    }
+    Collections.reverse(res);
+    return res;
+  }
+
+  public static void main(String[] args) {
+    TreeNode t1 = new TreeNode(5);
+    TreeNode t2 = new TreeNode(3);
+    TreeNode t3 = new TreeNode(8);
+    TreeNode t4 = new TreeNode(1);
+    TreeNode t5 = new TreeNode(4);
+    TreeNode t6 = new TreeNode(11);
+    t1.left = t2;
+    t1.right = t3;
+    t2.left = t4;
+    t2.right = t5;
+    t3.right = t6;
+    List<Integer> res = postOrder(t1);
+    System.out.println(res);
+  }
+}
