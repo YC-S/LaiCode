0ddb98d6ef0d56d4a93bf88effe81b0244233843
YC-S <yuanchenshi@gmail.com>
2021-07-07 21:17:33 -0700

    Time Count

---
+++ src/small_class/linear/TimeCount.java
@@ -0,0 +1,23 @@
+package small_class.linear;
+
+public class TimeCount {
+  public int timeForTask(String str, int k, int task) {
+    return 0;
+    //    Map<Task, Integer> taskTime = new HashMap<>();
+    //    Queue<Task> window = new LinkedList<>();
+    //    int timeCount = 0;
+    //    for (Task t : tasks) {
+    //      Integer lastTime = taskTime.get(t);
+    //      if (lastTime == null) {
+    //        timeCount++;
+    //      } else {
+    //        timeCount = lastTime + k + 1;
+    //      }
+    //      while (!window.isEmpty() && timeCount - taskTime.get(window.peek()) >= k) {
+    //        taskTime.remove(window.poll());
+    //      }
+    //      window.offer(t);
+    //      taskTime.put(t, timeCount);
+    //    }
+  }
+}
